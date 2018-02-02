178ff2abee3b510275d887eff4e97255b33cc40d
michael <zheng.che.hz@gmail.com>
2018-02-03 01:10:30 +0800

    Add H2 data base support.

---
+++ java/web/peopleana/src/main/resources/schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE people ( id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, first_name varchar(128) NOT NULL, last_name varchar(128) NOT NULL, is_male boolean NOT NULL, birthday date DEFAULT NULL, tel_no char(32) DEFAULT NULL, email char(128) DEFAULT NULL, mail_addr varchar(256) DEFAULT NULL)
